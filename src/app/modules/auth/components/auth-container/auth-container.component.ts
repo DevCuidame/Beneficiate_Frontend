b2fc0622c6c63bf3b53dd342ef607bd381d55bef
{ocardozo@cuidame.tech.com} <ocardozo@cuidame.tech>
2025-02-17 16:25:49 -0500

    Avance del cambio de contraseña

--- src/app/modules/auth/components/auth-container/auth-container.component.ts
+++ src/app/modules/auth/components/auth-container/auth-container.component.ts
@@ -3,23 +3,24 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { LoginComponent } from '../../pages/login/login.component';
 import { RegisterComponent } from '../../pages/register/register.component';
+import { ResetPasswordComponent } from '../../pages/reset-password/reset-password.component';
 import { TabBarComponent } from 'src/app/shared/components/tab-bar/tab-bar.component';
 
 @Component({
   selector: 'app-auth-container',
   standalone: true,
-  imports: [CommonModule, IonicModule, LoginComponent, RegisterComponent, TabBarComponent],
+  imports: [CommonModule, IonicModule, LoginComponent, RegisterComponent, ResetPasswordComponent, TabBarComponent],
   templateUrl: './auth-container.component.html',
   styleUrls: ['./auth-container.component.scss']
 })
 export class AuthContainerComponent {
-  isLoginView = true; 
+  isLoginView = true;
 
   toggleView() {
     this.isLoginView = !this.isLoginView;
   }
 
   switchToLogin() {
-    this.isLoginView = true; 
+    this.isLoginView = true;
   }
 }
